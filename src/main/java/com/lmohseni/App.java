0415f444fc1f3b5b50452f23fb5f8cd1a746a2ba
Logan Mohseni <mohsenil85@gmail.com>
2020-01-16 16:08:03 -0700

    implement processImage

--- src/main/java/com/lmohseni/App.java
+++ src/main/java/com/lmohseni/App.java
@@ -3,13 +3,13 @@ package com.lmohseni;
 import lombok.Data;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class App {
@@ -24,17 +24,54 @@ public class App {
                 new URL(url).openStream()));
     }
 
-    ColorProcessingResult processImage(BufferedImage image) {
-        return null;
+    ColorProcessingResult processImage(String url) {
+        try {
+            final BufferedImage image = downloadImage(url);
+            final HashMap<String, Integer> occurrences = new HashMap<>();
+            for (int x = 0; x < image.getWidth(); x++) {
+                for (int y = 0; y < image.getHeight(); y++) {
+                    final String rgb = Integer.toHexString(image.getRGB(x, y)).substring(0,6);
+                    if (occurrences.get(rgb) == null) {
+                        occurrences.put(rgb, 1);
+                    } else {
+                        int existing = occurrences.get(rgb);
+                        occurrences.put(rgb, existing + 1);
+                    }
+                }
+            }
+            Map.Entry<String, Integer> color1 = new AbstractMap.SimpleEntry<>("", 0);
+            Map.Entry<String, Integer> color2 = new AbstractMap.SimpleEntry<>("", 0);
+            Map.Entry<String, Integer> color3 = new AbstractMap.SimpleEntry<>("", 0);
+
+            for (Map.Entry<String, Integer> entry : occurrences.entrySet()) {
+                if (entry.getValue() > color1.getValue()) {
+                    color3 = color2;  //propagate runners up, order matters
+                    color2 = color1;
+                    color1 = entry;
+                }
+            }
+
+            return new ColorProcessingResult(
+                url,
+                color1.getKey(),
+                color2.getKey(),
+                color3.getKey()
+            );
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        throw new RuntimeException("Not supposed to happen");
     }
 
     @Data
     class ColorProcessingResult {
 
-        String imageUrl;
-        Color color1;
-        Color color2;
-        Color color3;
+        private final String imageUrl;
+        private final String color1;
+        private final String color2;
+        private final String color3;
     }
 
 }
