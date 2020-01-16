e9702dc0bfbbbc85a658a386b4dedf53f963c46f
Logan Mohseni <mohsenil85@gmail.com>
2020-01-16 11:16:44 -0700

    add downloadImage function

--- src/main/java/com/lmohseni/App.java
+++ src/main/java/com/lmohseni/App.java
@@ -1,5 +1,9 @@
 package com.lmohseni;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+
 /**
  * Hello world!
  */
@@ -7,6 +11,12 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    BufferedInputStream downloadImage(String url) throws IOException {
+
+        return new BufferedInputStream(new URL(url).openStream());
+
     }
 
 }
--- src/test/java/com/lmohseni/AppTest.java
+++ src/test/java/com/lmohseni/AppTest.java
@@ -2,11 +2,22 @@ package com.lmohseni;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
 public class AppTest {
 
-    @Test
-    public void main() {
-        App.main(new String[]{"foo"});
+    App app = new App();
+
+    @Test(expected = IOException.class)
+    public void downloadImageInvalid() throws IOException {
+        app.downloadImage("invalidHttp");
+    }
+
+    @Test(expected = IOException.class)
+    public void downloadImageNonexistant() throws IOException {
+        app.downloadImage("http://foo.example");
     }
 
 }
