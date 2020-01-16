bdeebbef7e34ff947d42917fadb34ff891bde57f
Logan Mohseni <mohsenil85@gmail.com>
2020-01-16 13:50:05 -0700

    add unit tests and testing utils

--- src/test/java/com/lmohseni/AppTest.java
+++ src/test/java/com/lmohseni/AppTest.java
@@ -1,7 +1,12 @@
 package com.lmohseni;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
@@ -9,6 +14,21 @@ import static org.junit.Assert.assertTrue;
 public class AppTest {
 
     App app = new App();
+
+    final String referenceImageUrl = "http://i.imgur.com/FApqk3D.jpg";
+
+    final File referenceImage = new File("src/test/resources/test-image.jpg");
+
+    @Before
+    public void setup(){
+        if(!referenceImage.exists()){
+            try {
+                retrieveReferenceImage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Test(expected = IOException.class)
     public void downloadImageInvalid() throws IOException {
@@ -18,6 +38,32 @@ public class AppTest {
     @Test(expected = IOException.class)
     public void downloadImageNonexistant() throws IOException {
         app.downloadImage("http://foo.example");
+    }
+
+    @Test
+    public void downloadImage() throws IOException {
+        final BufferedInputStream imageBytes = app
+            .downloadImage(referenceImageUrl);
+        BufferedImage actual = ImageIO.read(imageBytes);
+        final BufferedImage expected = ImageIO.read(referenceImage);
+        assertTrue(areImagesEqual(expected, actual));
+    }
+
+    private boolean areImagesEqual(BufferedImage expected, BufferedImage actual) {
+        //quick and dirty way to compare images, relies on java serialization
+        final int ignoredLength = 22;  //ignore the literal class name
+        return
+            expected.toString().substring(ignoredLength)
+                .equals(actual.toString().substring(ignoredLength));
+    }
+
+    //@Test
+    private void retrieveReferenceImage() throws IOException {
+        final BufferedInputStream imageBytes = app
+            .downloadImage(referenceImageUrl);
+        BufferedImage image = ImageIO.read(imageBytes);
+        ImageIO.write(image, "JPEG", referenceImage);
+
     }
 
 }
