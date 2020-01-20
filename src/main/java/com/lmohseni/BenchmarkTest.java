b727f0d0246c72895d4c46062a68a14a87d265c1
Logan Mohseni <mohsenil85@gmail.com>
2020-01-20 09:55:35 -0700

    add test annot to benchmarks so that they can be run from ide

--- src/main/java/com/lmohseni/BenchmarkTest.java
+++ src/main/java/com/lmohseni/BenchmarkTest.java
@@ -1,5 +1,6 @@
 package com.lmohseni;
 
+import org.junit.jupiter.api.Test;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -12,7 +13,7 @@ import org.openjdk.jmh.annotations.Warmup;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-@Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx8G"}, warmups = 2)
+@Fork(value = 2, jvmArgs = {"-Xms8G", "-Xmx12G"}, warmups = 2)
 @OutputTimeUnit(TimeUnit.SECONDS)
 @BenchmarkMode(Mode.All)
 @Warmup(iterations = 2)
@@ -27,10 +28,10 @@ public class BenchmarkTest {
         static String github =
             "https://gist.githubusercontent.com/ehmo/e736c827ca73d84581d812b3a27bb132/raw/77680b283d7db4e7447dbf8903731bb63bf43258/input.txt";
         static String testOutput = "./target/results.csv";
-        static int timeout = 5;
-        static int nThreads = 50;
+        static int timeout = 3;
+        static int nThreads = 10;
         static int colorCount = 3;
-        static int quality = 3;
+        static int quality = 50;
         static boolean ignoreWhite = false;
 
     }
@@ -44,6 +45,7 @@ public class BenchmarkTest {
 
 
     @Benchmark
+    @Test
     public void BenchProcessingTasks() {
 
         ProcessingTask.builder()
@@ -58,6 +60,7 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testProcessingTask10() {
 
         ProcessingTask.builder()
@@ -72,6 +75,7 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testProcessingTask30() {
 
         ProcessingTask.builder()
@@ -85,9 +89,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorForkJoinLocal() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.localUrl)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newWorkStealingPool())
@@ -97,9 +105,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorFixedLocal() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.localUrl)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newFixedThreadPool(Scope.nThreads))
@@ -109,9 +121,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorSingleThreadedLocal() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.localUrl)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newSingleThreadExecutor())
@@ -121,9 +137,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorCachedLocal() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.localUrl)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newCachedThreadPool())
@@ -133,9 +153,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorForkJoinRemote() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.github)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newWorkStealingPool())
@@ -145,9 +169,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorForkJoinRemote25() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.github)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newWorkStealingPool(25))
@@ -157,9 +185,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorForkJoinRemote200() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.github)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newWorkStealingPool(200))
@@ -169,9 +201,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorForkJoinRemote1000() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.github)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newWorkStealingPool(1000))
@@ -181,9 +217,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorFixedRemote50() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.github)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newFixedThreadPool(Scope.nThreads))
@@ -193,9 +233,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorFixedRemote200() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.github)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newFixedThreadPool(200))
@@ -205,9 +249,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorFixedRemote100() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.github)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newFixedThreadPool(1000))
@@ -217,9 +265,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorCachedRemote() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.github)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newCachedThreadPool())
@@ -229,9 +281,13 @@ public class BenchmarkTest {
     }
 
     @Benchmark
+    @Test
     public void testImageProcessorSingleThreadedRemote() {
         ImageProcessor.builder()
             .timeout(Scope.timeout)
+            .colorCount(Scope.colorCount)
+            .quality(Scope.quality)
+            .ignoreWhite(Scope.ignoreWhite)
             .imageListUrl(Scope.github)
             .outputFilePath(Scope.testOutput)
             .executorService(Executors.newSingleThreadExecutor())
