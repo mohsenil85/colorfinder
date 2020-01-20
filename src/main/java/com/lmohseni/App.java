ca2a106d8d372fb41d11a722d42e3d4d3a789dd7
Logan Mohseni <mohsenil85@gmail.com>
2020-01-20 10:24:08 -0700

    add whitespace

--- src/main/java/com/lmohseni/App.java
+++ src/main/java/com/lmohseni/App.java
@@ -6,19 +6,31 @@ import java.util.concurrent.Executors;
 public class App {
 
     public static void main(String[] args) {
+
         final String inputFile =
             "https://gist.githubusercontent.com/ehmo/e736c827ca73d84581d812b3a27bb132/raw/77680b283d7db4e7447dbf8903731bb63bf43258/input.txt";
 
         ImageProcessor.builder() //uses lombok.Builder
+
             .timeout(3) //idle seconds to wait before shutting down
+
             .colorCount(3) //per spec
+
             .quality(50) //tune the tradeoff between fidelity and speed
+
             .ignoreWhite(false)
+
             .imageListUrl(inputFile) //url pointing to list of images
-            // (for a local file, use a path like 'file:./some/local.csv')
-            .outputFilePath("./target/results.csv") //where to print the results
-            .executorService(Executors
-                .newWorkStealingPool()) //what type of threading strategy to use (see benchmarks)
+                                     // (for a local file, use a path like
+                                     // 'file:./some/local.csv')
+            .outputFilePath(
+                "./target/results.csv"
+            ) //where to print the results
+
+            .executorService(
+                Executors.newWorkStealingPool()
+            ) //what type of threading strategy to use (see benchmarks)
+
             .build()
 
             .processAllImages(); //do it!
