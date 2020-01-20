84cf46768fdc776acf771fdc9fc706b66ccad002
Logan Mohseni <mohsenil85@gmail.com>
2020-01-19 19:25:21 -0700

    whitespace cleanup

--- src/main/java/com/lmohseni/App.java
+++ src/main/java/com/lmohseni/App.java
@@ -6,17 +6,15 @@ import java.util.concurrent.Executors;
 public class App {
 
     public static void main(String[] args) {
-        ImageProcessor.builder() //uses lombok.Builder
+        ImageProcessor.builder()        //uses lombok.Builder
             .compressionPercentage(.1f) //amount to scale down the image by
-            .timeout(10) //idle seconds to wait before shutting down
-            .imageListUrl(
-                //url pointing to list of images (for a local file,
-                // use a path like 'file:./some/local.csv')
+            .timeout(10)                //idle seconds to wait before shutting down
+            .imageListUrl(              //url pointing to list of images (for a local file, use a path like 'file:./some/local.csv')
                 "https://gist.githubusercontent.com/ehmo/e736c827ca73d84581d812b3a27bb132/raw/77680b283d7db4e7447dbf8903731bb63bf43258/input.txt")
             .outputFilePath("./target/results.csv")  //where to print the results
             .executorService(Executors.newWorkStealingPool()) //what type of threading strategy to use (see benchmarks)
             .build()
 
-            .processAllImages();  //do it!
+            .processAllImages();        //do it!
     }
 }
